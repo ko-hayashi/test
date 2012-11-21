6e65fba1f33d53715a4dbb5b7e2294d5beefd9c2
unknown <hironori-kobayashi@ZZD100.coresoft.local>
2012-11-21 10:02:45 +0900

    to v1.1

--- Form.cs
+++ Form.cs
@@ -1,6 +1,9 @@
 #include	<stdio.h>
 
 
+#define	APL_S_SUCCESS	0x00000000
+
+
 extern	int	func_a();
 static	int	func_b();
 
@@ -13,6 +16,8 @@ int	main()
 	for(i = 0; i < 100; i ++){
 		printf("loop %d\n", i);
 	}
+	
+	return	APL_S_SUCCESS;
 }
 
 static	int	func_b()
